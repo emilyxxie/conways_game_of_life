3d47ce1448973c02320fe566f8aeeef0388931f0
emilyxxie <emilyxxie@gmail.com>
2016-04-08 20:36:53 -0400

    count neighbors properly

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 var cells = [];
-var cellsBuffer = [];
+var cellsBuffer = []; // a buffer to compare states against
 var screenWidth = 800;//screen.availWidth;
 var screenHeight = 400;//screen.availHeight;
 var cellSize = 40;
@@ -16,11 +16,35 @@ function draw() {
   var b = 0;
   cells.forEach(function(row, row_index) {
     row.forEach(function(cell, column_index) {
+      applyLifeRules(cell);
       fill(cell.r, cell.g, cell.b);
       rect(cell.x, cell.y, cellSize, cellSize);
     });
-
   });
+}
+
+function applyLifeRules(cell) {
+  var totalNeighbors = 0;
+
+  function _determineAlive(row, column) {
+    if (cells[row] && cells[row][column]) {
+      var neighborCell = cells[row][column];
+      if (neighborCell.alive) {
+        cell.neighbors++;
+      }
+    }
+  }
+
+
+  _determineAlive(cell.row - 1, cell.column - 1);  // top left
+  _determineAlive(cell.row - 1, cell.column);      // top
+  _determineAlive(cell.row - 1, cell.column + 1);  // top right
+  _determineAlive(cell.row, cell.column + 1);      // right
+  _determineAlive(cell.row + 1, cell.column + 1);  // bottom right
+  _determineAlive(cell.row + 1, cell.column);      // bottom
+  _determineAlive(cell.row + 1, cell.column - 1);  // bottom left
+  _determineAlive(cell.row, cell.column - 1);      // left
+
 }
 
 function createCellGrid() {
@@ -35,9 +59,11 @@ function createCellGrid() {
     // j corresponds to which column
     var x = 0;
     cells.push([]);
+    cellsBuffer.push([]);
     for (var j = 0; j < columns; j++) {
       cell = new Cell(x, y, i, j);
       cells[i].push(cell);
+      cellsBuffer.push(cell);
       x += cellSize;
     }
     y += cellSize;
@@ -50,13 +76,12 @@ function Cell(x, y, row, column) {
   this.row = row;
   this.column = column;
   this.alive = random(1, 100) <= aliveProbability;
+
+  // set colors for drawing
   this.r = 0;
   this.g = 0;
-
-  if (this.alive) {
-    this.b = 100;
-  } else {
-    this.b = 0;
-  }
-
+  this.b = this.alive ? 100 : 0;
+  this.neighbors = 0;
 }
+
+
